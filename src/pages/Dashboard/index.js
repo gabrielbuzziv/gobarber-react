c492936872f7d75ff72c0d9b4a46637a9dd465c7
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2020-01-13 16:00:15 -0300

    project init

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,100 @@
-import React from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setMinutes,
+  setHours,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
+
+import pt from 'date-fns/locale/pt';
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import api from '~/services/api';
+
+import { Container, Time } from './styles';
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [schedule, setSchedule] = useState([]);
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "dd 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  useEffect(() => {
+    async function loadSchedule() {
+      // const d1 = '2019-11-13 12:00:00 -0300';
+      // const d2 = '2019-11-13 12:00:00 -0300';
+
+      // console.log(isEqual(parseISO(d1), parseISO(d2)));
+
+      const response = await api.get('schedules', { params: { date } });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      console.log(response.data);
+
+      const data = range.map(hour => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timezone);
+        const formattedDate = format(checkDate, 'yyyy-MM-dd HH:mm:ss zzzz');
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(appointment => {
+            return isEqual(parseISO(formattedDate), parseISO(appointment.date));
+          }),
+        };
+      });
+
+      setSchedule(data);
+    }
+
+    loadSchedule();
+  }, [date]);
+
+  function handlePreviousDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePreviousDay}>
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+
+        <strong>{dateFormatted}</strong>
+
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Em aberto'}
+            </span>
+          </Time>
+        ))}
+      </ul>
+    </Container>
+  );
 }
--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth/reducers';
+import user from './user/reducers';
 
-export default combineReducers({ auth });
+export default combineReducers({ auth, user });
